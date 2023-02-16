ff8128de376f8a2ac3e9f5b375cf17cd855eac5e
Paulo Andrés Camacho Solimán <85325313+KelevsID@users.noreply.github.com>
2023-02-16 00:51:45 -0500

    Ejercicios tema 4

--- src/introduccion/tema4/Switch.java
+++ src/introduccion/tema4/Switch.java
@@ -2,10 +2,11 @@ package introduccion.tema4;
 
 /**
  * 
- * @author KelevsID 
- * 		   Por último, para el Switch, deberás crear la variable estacion, y distintos case para las cuatro estaciones del año.
- *         Dependiendo del valor de la variable estacion se deberá mandar un mensaje por consola informando de la estación 
- *         en la que está. También habrá que poner un default para cuando el valor de la variable no sea una estación.
+ * @author KelevsID
+ *		Ejercicios tema 4 
+ * 		Por último, para el Switch, deberás crear la variable estacion, y distintos case para las cuatro estaciones del año.
+ *      Dependiendo del valor de la variable estacion se deberá mandar un mensaje por consola informando de la estación 
+ *      en la que está. También habrá que poner un default para cuando el valor de la variable no sea una estación.
  */
 public class Switch {
 	public static void main(String[] args) {
